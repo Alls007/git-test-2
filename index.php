55e9491093b480f0ecd49355d981b7b17e961783
aliyunusjuan <aliyunusjuan@gmail.com>
2024-06-02 10:45:09 +0700

    menambahkan list

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat aut vero labore eum minus error repellendus unde cum libero! Perferendis modi omnis esse aliquam. Aperiam nihil at natus doloribus eius.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
+</body>
+</html>
